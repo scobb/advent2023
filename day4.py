ad194f38c167db6d9062fd7f550d5c28f3c7c52c
Steve Cobb <scobb@alumni.lsu.edu>
2023-12-18 20:21:37 -0800

    4a

--- day4.py
+++ day4.py
@@ -7,7 +7,19 @@ if __name__ == '__main__':
 
 
 def part_a(infile: TextIO) -> str:
-    pass
+    # (1 for the first match, then doubled three times for each of the three matches after the first)
+    result = 0
+    for line in infile.readlines():
+        nums = line.split(':')[1]
+        winning, mine = nums.split('|')
+        winning_set = set(map(int, winning.split()))
+        my_set = set(map(int, mine.split()))
+        matches = len(winning_set & my_set)
+        if matches > 0:
+            result += 1 << (matches-1)
+    return str(result)
+
+
 
 
 def part_b(infile: TextIO) -> str:
