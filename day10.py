0e63a2019db9fdb7a0665daeb10f7f3b02f0400f
Steve Cobb <scobb@alumni.lsu.edu>
2023-12-26 13:38:52 -0800

    10 boilerplate

---
+++ day10.py
@@ -0,0 +1,15 @@
+
+from util import harness
+from typing import TextIO
+
+if __name__ == '__main__':
+    harness(10)
+
+
+def part_a(infile: TextIO) -> str:
+    pass
+
+
+def part_b(infile: TextIO) -> str:
+    pass
+
