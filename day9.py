129a6e558ec3b30b03ff88d7b05a0504e401c68f
Steve Cobb <scobb@alumni.lsu.edu>
2023-12-26 13:36:10 -0800

    9b

--- day9.py
+++ day9.py
@@ -24,6 +24,20 @@ def part_a(infile: TextIO) -> str:
     return str(result)
 
 
+def find_prev(arr: List[int]) -> int:
+    if all(x == 0 for x in arr):
+        return 0
+    deltas = []
+    for idx in range(len(arr)-1):
+        deltas.append(arr[idx+1]-arr[idx])
+    return arr[0] - find_prev(deltas)
+
 def part_b(infile: TextIO) -> str:
-    pass
+    result = 0
+    for line in infile.readlines():
+        if not line.strip():
+            continue
+        arr = list(map(int, line.split()))
+        result += find_prev(arr)
+    return str(result)
 
