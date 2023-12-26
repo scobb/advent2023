70c012626256ba5021b30fa196b1f17a979e2336
Steve Cobb <scobb@alumni.lsu.edu>
2023-12-26 13:34:13 -0800

    9a

--- day9.py
+++ day9.py
@@ -1,13 +1,27 @@
 
 from util import harness
-from typing import TextIO
+from typing import TextIO, List
 
 if __name__ == '__main__':
     harness(9)
 
+def find_next(arr: List[int]) -> int:
+    if all(x == 0 for x in arr):
+        return 0
+    deltas = []
+    for idx in range(len(arr)-1):
+        deltas.append(arr[idx+1]-arr[idx])
+    return arr[-1] + find_next(deltas)
+
 
 def part_a(infile: TextIO) -> str:
-    pass
+    result = 0
+    for line in infile.readlines():
+        if not line.strip():
+            continue
+        arr = list(map(int, line.split()))
+        result += find_next(arr)
+    return str(result)
 
 
 def part_b(infile: TextIO) -> str:
