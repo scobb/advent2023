a73273e1935ce970dc38711480d56a379c700756
Steve Cobb <scobb@alumni.lsu.edu>
2023-12-28 09:27:51 -0800

    11b

--- day11.py
+++ day11.py
@@ -14,6 +14,8 @@ In fact, the result is that any rows or columns that contain no galaxies
 should all actually be twice as big.
 """
 def part_a(infile: TextIO) -> str:
+    return common(infile, 2)
+def common(infile: TextIO, expansion_factor: int) -> str:
     galaxies = []
     galaxy_ys = set()
     galaxy_xs = set()
@@ -35,21 +37,21 @@ def part_a(infile: TextIO) -> str:
 
     # print(galaxies, missing_ys, missing_xs)
     result = 0
+    # print(missing_ys, missing_xs)
     for idxa in range(len(galaxies)):
         for idxb in range(idxa+1, len(galaxies)):
-
             g1 = galaxies[idxa]
             g2 = galaxies[idxb]
             y_range = sorted([g1[0], g2[0]])
             x_range = sorted([g1[1], g2[1]])
             skipped_ys = bisect.bisect_left(missing_ys, y_range[1]) - bisect.bisect_left(missing_ys, y_range[0])
             skipped_xs = bisect.bisect_left(missing_xs, x_range[1]) - bisect.bisect_left(missing_xs, x_range[0])
-            # print(y_range, skipped_ys)
+            # print(y_range, missing_ys, skipped_ys, x_range, missing_xs, skipped_xs)
             # print(x_range, skipped_xs)
-            result += abs(g1[0]-g2[0]) + abs(g1[1]-g2[1]) + skipped_xs + skipped_ys
+            result += abs(g1[0]-g2[0]) + abs(g1[1]-g2[1]) + (skipped_xs + skipped_ys)*(expansion_factor-1)
     return str(result)
 
 
 def part_b(infile: TextIO) -> str:
-    pass
+    return common(infile, 1000000)
 
