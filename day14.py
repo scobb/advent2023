3ae88241359f41714b03940be7e58575d81642c9
Steve Cobb <scobb@alumni.lsu.edu>
2024-01-06 17:21:49 -0800

    very close w/ detecting the cycle, but something is off to not match expected val on sample

--- day14.py
+++ day14.py
@@ -1,6 +1,7 @@
 
 from util import harness
-from typing import TextIO
+from typing import TextIO, Tuple, List
+from functools import cache
 from collections import defaultdict
 
 if __name__ == '__main__':
@@ -8,28 +9,80 @@ if __name__ == '__main__':
 
 
 def part_a(infile: TextIO) -> str:
-    dp = None
-    rocks = defaultdict(int)
+    return common(infile, 1)
+
+@cache
+def slide(rocks: Tuple[Tuple[int, int, str]]) -> Tuple[Tuple[int,int,str]]:
+    result = []
+    dp = defaultdict(int)
+    for rock in rocks:
+        row_idx, col_idx, c = rock
+        if c == 'O':
+            result.append((dp[col_idx], col_idx, c))
+            dp[col_idx] += 1
+        else:
+            result.append((row_idx, col_idx, c))
+            dp[col_idx] = row_idx + 1
+    return tuple(result)
+
+@cache
+def rotate(rocks: Tuple[Tuple[int, int, str]], num_cols: int) -> Tuple[Tuple[int,int,str]]:
+    # TODO - implement
+    return tuple((col_idx, num_cols - row_idx - 1, c) for (row_idx, col_idx, c) in rocks)
+
+
+def print_map(rocks: Tuple[Tuple[int, int, str]], num_rows: int, num_cols: int):
+    rock_map = {(row_idx, col_idx): c for (row_idx, col_idx, c) in rocks}
+    result = []
+    for row_idx in range(num_rows):
+        result.append('')
+        for col_idx in range(num_cols):
+            result[row_idx] += rock_map.get((row_idx, col_idx), '.')
+    print('\n'.join(result))
+
+
+def common(infile: TextIO, rotations: int) -> str:
+    rocks = []
     row_cnt = 0
+
     for row_idx, line in enumerate(infile.readlines()):
         ls = line.strip()
         if ls:
             row_cnt += 1
-            dp = dp or [0 for _ in ls]
+            col_cnt = len(ls)
             for col_idx, c in enumerate(ls):
-                if c == 'O':
-                    rocks[dp[col_idx]] += 1
-                    dp[col_idx] += 1
-                elif c == '#':
-                    dp[col_idx] = row_idx+1
-
-    print(dp)
-    print(rocks)
-    print(row_cnt)
-    result = sum((row_cnt - k) * v for k, v in rocks.items())
-    return str(result)
+                if c in ('O', '#'):
+                    rocks.append((row_idx, col_idx, c))
+    results = defaultdict(list)
+    directions = ('n', 'w', 's', 'e')
+    for idx in range(rotations):
+        # print(idx)
+        rocks = slide(tuple(rocks))
+        if idx == rotations - 1:
+            result = sum((row_cnt - r[0]) for r in rocks if r[2] == 'O')
+            return str(result)
+        results[rocks].append(idx)
+        if len(results[rocks]) > 3:
+            cycle_length_a = results[rocks][-1] - results[rocks][-2]
+            cycle_length_b = results[rocks][-2] - results[rocks][-3]
+            # if cycle_length_a == cycle_length_b:
+            if cycle_length_a == cycle_length_b and (rotations - idx) % cycle_length_a == 0:
+                print(directions[idx % len(directions)])
+                print(idx, rotations, cycle_length_a, row_cnt, rocks)
+                # get back to north orientation
+                rocks = rotate(rocks, row_cnt)
+                print_map(rocks, row_cnt, col_cnt)
+                result = sum((row_cnt - r[0]) for r in rocks if r[2] == 'O')
+                return str(result)
+            print(results[rocks])
+        print('BEFORE ROTATION')
+        print_map(rocks, row_cnt, col_cnt)
+        rocks = rotate(rocks, row_cnt)
+        print('AFTER ROTATION')
+        print_map(rocks, row_cnt, col_cnt)
+    # print(sorted(rocks))
 
 
 def part_b(infile: TextIO) -> str:
-    pass
+    return common(infile, 4*1000000000 - 1)
 
